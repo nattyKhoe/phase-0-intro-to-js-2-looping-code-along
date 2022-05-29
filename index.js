33d781ffffc19ebca5f0dbe64ff6df56efe5699d
nattyKhoe <natasha.kusuma@outlook.com>
2022-05-29 12:02:04 +1000

    Done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name,eventName){
+    let message = [];
+    for (let i=0; i<name.length; i++){
+        message[i] = `Thank you, ${name[i]}, for the wonderful ${eventName} gift!`;
+    }
+    return message;
+}
+
+function countDown (n){
+    while (n>=0){
+        console.log(n);
+        n--;
+    }
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
